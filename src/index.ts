d7098303ce8b8371654480ef385f3ea3d5815111
Anshu Rai <raianshu8991@gmail.com>
2021-03-15 18:39:34 +0530

    added error handler middleware & custom error and custom error handler

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express  from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
 import routes from './routes';
+import {errorHandler} from './middleware/error-handler';
+import {NotFoundError} from './errors/404-error';
 const port =process.env.PORT || 3000;
 const app=express();
 app.use(json());
+app.use(errorHandler)
+
 
 // using routes 
 app.use('/',routes);
+
+
 app.listen(port,()=>{
     console.log(`server is up & running at ${port} !!` );
 })
